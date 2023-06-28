646b85d0cca21789b1ef7412876fea62f5fb13a2
Yasas <118436384+Yasashari@users.noreply.github.com>
2023-06-28 15:31:48 +0100

    Update maia3.sol

--- maia3.sol
+++ maia3.sol
@@ -1,1 +1,7 @@
+## Delegatecall dost check the contract existence . Since delegatecall return true even if the contract not existed.
+
+Low-level calls call/delegatecall/staticcall return true even if the account called is non-existent (per EVM design).
+Solidity documentation warns: "The low-level functions call, delegatecall and staticcall return true as their first return value
+if the account called is non-existent, as part of the design of the EVM. Account existence must be checked prior to calling if needed.”
+
 
